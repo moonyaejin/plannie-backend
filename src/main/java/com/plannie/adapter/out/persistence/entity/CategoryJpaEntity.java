86ca62b9c25174ff8be983e2e441d8442a3cc084
moonyaejin <i0209i80@gmail.com>
2025-12-05 12:05:49 +0900

    feat: 카테고리 Entity, Repository 추가

---
+++ src/main/java/com/plannie/adapter/out/persistence/entity/CategoryJpaEntity.java
@@ -0,0 +1,39 @@
+package com.plannie.adapter.out.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories", indexes = {
+        @Index(name = "idx_category_user_id", columnList = "user_id")
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CategoryJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "category_name", nullable = false, length = 10)
+    private String categoryName;
+
+    @Column(name = "color", length = 200)
+    private String color;
+
+    @Version
+    private Long version;
+
+    @Builder
+    public CategoryJpaEntity(Long userId, String categoryName, String color) {
+        this.userId = userId;
+        this.categoryName = categoryName;
+        this.color = color;
+    }
+}
