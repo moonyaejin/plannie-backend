3c4252c5ac6d98e364d4ea22183b28d9e33854ce
moonyaejin <i0209i80@gmail.com>
2026-04-10 13:39:15 +0900

    feat: JWT 의존성 추가 및 JwtProvider 구현

---
+++ src/main/java/com/plannie/security/JwtProperties.java
@@ -0,0 +1,6 @@
+package com.plannie.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(String secret, long expirationMs) {}
