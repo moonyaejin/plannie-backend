cc4e0b445d227e0ec6f730087423a2c0ba2f290f
moonyaejin <i0209i80@gmail.com>
2026-07-18 16:40:28 +0900

    fix: 반복 일정 수정/삭제 시 occurrence 단위 처리 지원

--- src/main/java/com/plannie/application/port/in/DeleteScheduleUseCase.java
+++ src/main/java/com/plannie/application/port/in/DeleteScheduleUseCase.java
@@ -1,11 +1,16 @@
 package com.plannie.application.port.in;
 
+import java.time.LocalDate;
+
 /**
  * 일정 삭제 유스케이스
  */
 public interface DeleteScheduleUseCase {
 
-    void deleteSchedule(Long scheduleId, Long userId);
+    /**
+     * @param occurrenceDate null = 시리즈 전체 삭제, non-null = 반복 일정의 해당 날짜 occurrence만 삭제
+     */
+    void deleteSchedule(Long scheduleId, Long userId, LocalDate occurrenceDate);
 
     void bulkDeleteSchedules(Long userId, Integer year, Integer month);
 }
